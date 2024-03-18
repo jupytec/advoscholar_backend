aae54ac18ba81ba97e8bd00d1e7feac31ecbfb4f
Destiny Saturday <yiradesat@gmail.com>
2024-03-18 16:14:20 +0100

    feat: basic school doctypes

---
+++ advoscholar/advoscholar/doctype/school_settings/school_settings.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Jupytec and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("School Settings", {
+//     refresh(frm) {
+//         frm.doc.email = "test@gmail.com"
+//         frm.refresh_field("email")
+//     },
+// });
---
+++ advoscholar/advoscholar/doctype/school_settings/school_settings.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2024, Jupytec and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class SchoolSettings(Document):
+	# def on_update(self):
+	# 	frappe.msgprint("School Settings Updated")
+	# 	pass
